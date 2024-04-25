50033f4e4b13b2e89199435e656c2d044eff9048
13219071 Reynaldo Averill Adji Putra <13219071@std.stei.itb.ac.id>
2024-04-26 05:35:58 +0700

    add page_anastomosis

---
+++ userinterface/page_anastomosis.py
@@ -0,0 +1,245 @@
+import logging
+from tkinter import Canvas, Entry, Text, Button, PhotoImage
+from .userinterface_tools import relative_to_assets
+
+page_name = "page_anastomosis"
+logger = logging.getLogger(__name__)
+
+class Page_anastomosis(Canvas):
+    def __init__(self,parent):
+        super().__init__(parent)
+        logger.debug("Create "+str(page_name)+" canvas")
+        self.config(
+            bg = "#F2F2F2",
+            height = 480,
+            width = 800,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        self.text_stopwatch_time = "00:00:00"
+        self.text_camera_connectionstatus = "DISCONNECTED"
+        self.text_camera_recordingstatus = "NOT RECORDING"
+
+        self.create_rectangle(
+            120.0,
+            345.0,
+            140.0,
+            385.0,
+            fill="#D9D9D9",
+            outline="")
+
+        self.create_rectangle(
+            300.0,
+            345.0,
+            320.0,
+            385.0,
+            fill="#D9D9D9",
+            outline="")
+
+        self.create_rectangle(
+            480.0,
+            345.0,
+            500.0,
+            385.0,
+            fill="#D9D9D9",
+            outline="")
+
+        self.create_rectangle(
+            660.0,
+            345.0,
+            680.0,
+            385.0,
+            fill="#D9D9D9",
+            outline="")
+
+        self.create_rectangle(
+            20.0,
+            150.0,
+            600.0,
+            260.0,
+            fill="#5DA295",
+            outline="")
+
+        self.create_rectangle(
+            620.0,
+            150.0,
+            780.0,
+            260.0,
+            fill="#5DA295",
+            outline="")
+
+        self.create_text(
+            120,205,
+            anchor="c",
+            text="TIME ELAPSED",
+            fill="#FFFFFF",
+            font=('Inter',-40,'bold'),
+            width=200,
+            justify="center"
+        )
+
+        self.create_text(
+            410,205,
+            anchor="c",
+            text=self.text_stopwatchtime,
+            fill="#FFFFFF",
+            font=('Inter', 60 * -1,'bold'),
+            width=380,
+            justify="center"
+        )
+
+        self.create_text(
+            620.0,
+            151.0,
+            anchor="nw",
+            text="CAMERA",
+            fill="#FFFFFF",
+            font=("Inter Bold", 30 * -1)
+        )
+
+        self.create_text(
+            620.0,
+            191.0,
+            anchor="nw",
+            text="DISCONNECTED",
+            fill="#FFFFFF",
+            font=("Inter Bold", 15 * -1)
+        )
+
+        self.create_text(
+            620.0,
+            227.0,
+            anchor="nw",
+            text="NOT RECORDING",
+            fill="#FFFFFF",
+            font=("Inter Bold", 15 * -1)
+        )
+
+        self.button_image_1 = PhotoImage(
+            file=relative_to_assets(page_name,"button_1.png"))
+        self.button_1 = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_1 clicked"),
+            relief="flat"
+        )
+        self.button_1.place(
+            x=20.0,
+            y=315.0,
+            width=100.0,
+            height=100.0
+        )
+
+        self.button_image_2 = PhotoImage(
+            file=relative_to_assets(page_name,"button_2.png"))
+        self.button_2 = Button(
+            image=self.button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        self.button_2.place(
+            x=680.0,
+            y=315.0,
+            width=100.0,
+            height=100.0
+        )
+
+        self.button_image_3 = PhotoImage(
+            file=relative_to_assets(page_name,"button_3.png"))
+        self.button_3 = Button(
+            image=self.button_image_3,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_3 clicked"),
+            relief="flat"
+        )
+        self.button_3.place(
+            x=140.0,
+            y=280.0,
+            width=160.0,
+            height=160.0
+        )
+
+        self.button_image_4 = PhotoImage(
+            file=relative_to_assets(page_name,"button_4.png"))
+        self.button_4 = Button(
+            image=self.button_image_4,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_4 clicked"),
+            relief="flat"
+        )
+        self.button_4.place(
+            x=320.0,
+            y=280.0,
+            width=160.0,
+            height=160.0
+        )
+
+        self.button_image_5 = PhotoImage(
+            file=relative_to_assets(page_name,"button_5.png"))
+        self.button_5 = Button(
+            image=self.button_image_5,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_5 clicked"),
+            relief="flat"
+        )
+        self.button_5.place(
+            x=500.0,
+            y=280.0,
+            width=160.0,
+            height=160.0
+        )
+
+        self.button_image_6 = PhotoImage(
+            file=relative_to_assets(page_name,"button_6.png"))
+        self.button_6 = Button(
+            image=self.button_image_6,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_6 clicked"),
+            relief="flat"
+        )
+        self.button_6.place(
+            x=20.0,
+            y=20.0,
+            width=240.0,
+            height=110.0
+        )
+
+        self.button_image_7 = PhotoImage(
+            file=relative_to_assets(page_name,"button_7.png"))
+        self.button_7 = Button(
+            image=self.button_image_7,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_7 clicked"),
+            relief="flat"
+        )
+        self.button_7.place(
+            x=280.0,
+            y=20.0,
+            width=240.0,
+            height=110.0
+        )
+
+        self.button_image_8 = PhotoImage(
+            file=relative_to_assets(page_name,"button_8.png"))
+        self.button_8 = Button(
+            image=self.button_image_8,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_8 clicked"),
+            relief="flat"
+        )
+        self.button_8.place(
+            x=540.0,
+            y=20.0,
+            width=240.0,
+            height=110.0
+        )
--- userinterface/userinterface.py
+++ userinterface/userinterface.py
@@ -1,6 +1,7 @@
 import logging
 from .window import Window
 from .page_main import Page_main
+from .page_anastomosis import Page_anastomosis
 
 logger = logging.getLogger(__name__)
 
@@ -8,15 +9,16 @@ class Userinterface:
     def __init__(self,title,dimension):
         self.window = Window(title,dimension)
         self.page_classes = {
-            "page_main" : Page_main
+            "page_main" : Page_main,
+            "page_anastomosis" : Page_anastomosis
         }
         self.current_page = None
     
     def switch_to(self, name):
         logger.debug("Switch to "+str(name))
         new_frame = self.page_classes[name](self.window)
-        # if self.current_page is not None:
-            # self.current_page.place_forget()
+        if self.current_page is not None:
+            self.current_page.place_forget()
         self.current_page = new_frame
         self.current_page.place(x=0, y=0)
 
