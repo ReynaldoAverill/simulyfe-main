d8a463494d3dec6e0118bebb023e7e69c7389cad
13219071 Reynaldo Averill Adji Putra <13219071@std.stei.itb.ac.id>
2024-04-26 05:12:48 +0700

    fix bug ui not showing by changing image into attributes

--- userinterface/page_main.py
+++ userinterface/page_main.py
@@ -1,7 +1,9 @@
 
+from pathlib import Path
 import logging
 from tkinter import Canvas, Entry, Text, Button, PhotoImage
 from .userinterface_tools import relative_to_assets
+
 page_name = "page_main"
 logger = logging.getLogger(__name__)
 
@@ -18,67 +20,68 @@ class Page_main(Canvas):
             relief = "ridge"
         )
 
-        self.place(x = 0, y = 0)
-
-        image_image_1 = PhotoImage(
+        # image_1
+        self.image_image_1 = PhotoImage(
             file=relative_to_assets(page_name,"image_1.png"))
-        image_1 = self.create_image(
+        self.create_image(
             400.0,
             240.0,
-            image=image_image_1
+            image=self.image_image_1
         )
 
-        # button_image_1 = PhotoImage(
-        #     file=relative_to_assets(page_name,"button_1.png"))
-        # button_1 = Button(self,
-        #     image=button_image_1,
-        #     borderwidth=0,
-        #     highlightthickness=0,
-        #     # command=lambda: canvas2.place(x=0,y=0),
-        #     relief="flat"
-        # )
-        # button_1.place(
-        #     x=200.0,
-        #     y=205.0,
-        #     width=400.0,
-        #     height=100.0
-        # )
+        # button_start
+        self.button_image_1 = PhotoImage(
+            file=relative_to_assets(page_name,"button_1.png"))
+        self.button_start = Button(self,
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            # command=lambda: canvas2.place(x=0,y=0),
+            relief="flat"
+        )
+        self.button_start.place(
+            x=200.0,
+            y=205.0,
+            width=400.0,
+            height=100.0
+        )
 
-        button_image_2 = PhotoImage(
+        #  button_othermenu
+        self.button_image_2 = PhotoImage(
         file=relative_to_assets(page_name,"button_2.png"))
-
-        button_2b = Button(self,
-            image=button_image_2,
+        self.button_othermenu = Button(self,
+            image=self.button_image_2,
             borderwidth=0,
             highlightthickness=0,
             # command=lambda: self2.tkraise(),
             relief="flat"
         )
-        button_2b.place(
+        self.button_othermenu.place(
             x=200.0,
             y=346.0,
             width=400.0,
             height=100.0
         )
 
-        button_2b.config(command=lambda: print("button_3 clicked"))
+        self.button_othermenu.config(command=lambda: print("button_2 clicked"))
 
-        button_image_3 = PhotoImage(
+        self.button_image_3 = PhotoImage(
             file=relative_to_assets(page_name,"button_3.png"))
-        button_3 = Button(self,
-            image=button_image_3,
+        self.button_exit = Button(self,
+            image=self.button_image_3,
             borderwidth=0,
             highlightthickness=0,
             command=lambda: parent.destroy(),
             relief="flat"
         )
-        button_3.place(
+        self.button_exit.place(
             x=35.0,
             y=346.0,
             width=100.0,
             height=100.0
         )
         logger.debug(str(page_name)+" canvas created")
+        self.place(x = 0, y = 0)
         # parent.mainloop()
 
 
--- userinterface/userinterface.py
+++ userinterface/userinterface.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 class Userinterface:
     def __init__(self,title,dimension):
         self.window = Window(title,dimension)
-        self.page_main = Page_main(self.window)
         self.page_classes = {
             "page_main" : Page_main
         }
@@ -15,15 +14,11 @@ class Userinterface:
     
     def switch_to(self, name):
         logger.debug("Switch to "+str(name))
-        # new_frame = self.page_main
         new_frame = self.page_classes[name](self.window)
-        # self.start_mainloop()
-        logger.debug("created")
         # if self.current_page is not None:
             # self.current_page.place_forget()
         self.current_page = new_frame
         self.current_page.place(x=0, y=0)
-        self.start_mainloop()
 
     def start_mainloop(self):
         self.window.mainloop()
