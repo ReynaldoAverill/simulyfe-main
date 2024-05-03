2c51d527284b724b4ed9a8c1e527e2bbe7a6f5c1
13219071 Reynaldo Averill Adji Putra <13219071@std.stei.itb.ac.id>
2024-05-04 02:16:24 +0700

    back to successfully runned code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import logging
 import os
 
 # Set level for config
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.CRITICAL)
 logger = logging.getLogger(__name__)
 
 def main():
@@ -15,11 +15,11 @@ def main():
     if os.environ.get('DISPLAY','') == '':
         logger.debug('no display found. Using :0.0')
         os.environ.__setitem__('DISPLAY', ':0.0')
-    # model = Model()
-    # userinterface = Userinterface('Simulyfe Anastomosis Training Kit',(800,480))
-    # controller = Controller_userinterface(model,userinterface)
-    # controller.start_app()
-    start_stopwatch_test()
+    model = Model()
+    userinterface = Userinterface('Simulyfe Anastomosis Training Kit',(800,480))
+    controller = Controller_userinterface(model,userinterface)
+    controller.start_app()
+    # start_stopwatch_test()
 
     # userinterface.switch_to("page_confirmation_anastomosis_to_main")
     # userinterface.switch_to("page_main")
--- test_module/stopwatch_test.py
+++ test_module/stopwatch_test.py
@@ -1,25 +1,32 @@
 import tkinter as tk
+import threading
+import time
 
 def start_stopwatch_test():
     root = tk.Tk() 
     app = Application(window=root)
     app.mainloop()
 
-class Application(tk.Frame):
+class Application(tk.Canvas):
 
     def __init__(self, window=None):
         super().__init__(window)
         self.window = window
         self.update_time = ''
         self.running = False
+        
+        self.passed = 0
+        self.started = False
+
         self.hours = 0
         self.minutes = 0
         self.seconds = 0
+        self.miliseconds = 0
         self.pack()
         self.create_widgets()
 
     def create_widgets(self):
-        self.stopwatch_label = tk.Label(self, text='00:00:00', font=('Arial', 80))
+        self.stopwatch_label = tk.Text(self, text='00:00:00:00', font=('Arial', 80))
         self.stopwatch_label.pack()
         self.start_button = tk.Button(self, text='start', height=5, width=7, font=('Arial', 20), command=self.start)
         self.start_button.pack(side=tk.LEFT)
@@ -33,24 +40,49 @@ class Application(tk.Frame):
 
     def start(self):
         if not self.running:
-            self.stopwatch_label.after(1000)
-            self.update()
             self.running = True
+            threading.Thread(target=self.update_time_stopwatch).start()
+            self.start_button.config(state='disabled')
+            # self.stopwatch_label.after(1000)
+            # self.update()
 
     def pause(self):
         if self.running:
-            self.stopwatch_label.after_cancel(self.update_time)
+            # self.stopwatch_label.after_cancel(self.update_time)
             self.running = False
+            self.start_button.config(state='active')
 
     def reset(self):
         if self.running:
-            self.stopwatch_label.after_cancel(self.update_time)
+            # self.stopwatch_label.after_cancel(self.update_time)
             self.running = False
-        self.hours, self.minutes, self.seconds = 0, 0, 0
-        self.stopwatch_label.config(text='00:00:00')
+        self.start_button.config(state='active')
+        self.passed = 0
+        self.stopwatch_label.config(text=self.format_time_string(self.passed))
+        # self.hours, self.minutes, self.seconds = 0, 0, 0
+        # self.stopwatch_label.config(text='00:00:00')
+    
+    def format_time_string(self, time_passed):
+        seconds = time_passed % 60
+        minutes = time_passed // 60
+        hours   = minutes // 60
+        return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}:{int((self.passed)%1*100):02d}"
+
+    def update_time_stopwatch(self):
+        start = time.time()
+        if self.running:
+            until_now = self.passed
+        else:
+            until_now = 0
+        while self.running:
+            self.passed = time.time() - start + until_now
+            self.stopwatch_label.config(text=self.format_time_string(self.passed))
 
     def update(self):
+        # self.miliseconds += 1
+        # if self.miliseconds == 100:
         self.seconds += 1
+            # self.miliseconds = 0
         if self.seconds == 60:
             self.minutes += 1
             self.seconds = 0
@@ -60,6 +92,7 @@ class Application(tk.Frame):
         hours_string = f'{self.hours}' if self.hours > 9 else f'0{self.hours}'
         minutes_string = f'{self.minutes}' if self.minutes > 9 else f'0{self.minutes}'
         seconds_string = f'{self.seconds}' if self.seconds > 9 else f'0{self.seconds}'
+        miliseconds_string = f'{self.miliseconds}' if self.miliseconds > 9 else f'0{self.miliseconds}'
         self.stopwatch_label.config(text=hours_string + ':' + minutes_string + ':' + seconds_string)
+        # self.stopwatch_label.config(text=hours_string + ':' + minutes_string + ':' + seconds_string + ':' + miliseconds_string)
         self.update_time = self.stopwatch_label.after(1000, self.update)
-
