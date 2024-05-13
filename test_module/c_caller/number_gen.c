4465c86170783644b8b7ac4f9585ff156b2cd787
13219071 Reynaldo Averill Adji Putra <13219071@std.stei.itb.ac.id>
2024-05-13 23:57:01 +0700

    edit subprocess reader

--- test_module/c_caller/number_gen.c
+++ test_module/c_caller/number_gen.c
@@ -5,7 +5,7 @@
 
 int main(){
     int i = 0;
-    while(i<10){
+    while(i<3){
         printf("i=%d\n",i);
         i++;
         sleep(1);
--- test_module/c_caller/tes_subprocess.py
+++ test_module/c_caller/tes_subprocess.py
@@ -1,10 +1,17 @@
 from subprocess import Popen, PIPE
+import time
 
+create_exe  = ['gcc','-o','coba','number_gen.c']
 perintah    = [".\coba"]
 # perintah = "./coba"
-
-proc = Popen(perintah, stdout = PIPE, stderr = PIPE,text=True)
-
+proc1 = Popen(create_exe)
+proc1.wait()
+print("exe file created")
+proc = Popen(perintah, stdout = PIPE, stderr = PIPE,text=True,universal_newlines=True)
+print("open exe file")
+while (True):
+    print(proc.stdout.readline())
+    time.sleep(1)
 hasil, err = proc.communicate()
 
 # resp = hasil.decode("utf-8")
